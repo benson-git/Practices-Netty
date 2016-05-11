42229564d3c2cae40ac70a9562119150a96bd3e7
benson.wang <chikaiwang@hotmail.com>
2016-05-11 15:44:02 +0800

    project initialization

---
+++ NettyPractices/src/practices/nio/nature/Test.java
@@ -0,0 +1,19 @@
+package practices.nio.nature;
+
+public class Test {
+
+  public final static int OP_READ = 1 >> 0;
+  public final static int OP_WRITE = 1 >> 2;
+  public final static int OP_CONNECT = 1 >> 3;
+  public final static int OP_ACCEPT = 1 >> 4;
+  
+  public static void main(String[] args) {
+    
+    
+      System.out.println(OP_READ);
+      System.out.println(OP_WRITE);
+      System.out.println(OP_CONNECT);
+      System.out.println(OP_ACCEPT);
+  }
+
+}
